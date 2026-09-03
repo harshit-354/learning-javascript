1ea48b615dd094951a980b69276954351c5704bb
harshit <harshit.gupta0411@gmail.com>
2026-09-04 04:20:40 +0530

    datatypes

---
+++ basic/revise.js
@@ -0,0 +1,10 @@
+const accountId=163711
+let accountEmail="hitesh@google.com"
+var accountPassword="12342"
+let accountCity="faridabad"
+let accountState;
+
+// accountId=2
+
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
