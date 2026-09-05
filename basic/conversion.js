24c5d4983e0878de2a42c4ead98552f984963c50
harshit <harshit.gupta0411@gmail.com>
2026-09-06 04:30:34 +0530

    comparison

---
+++ basic/conversion.js
@@ -0,0 +1,10 @@
+let score=33
+console.log(typeof score)
+let number=33
+let stringNumber=String(number)
+console.log(stringNumber)
+
+console.log((3+5)*(8+9));
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter)
--- basic/dataTypes.js
+++ basic/dataTypes.js
@@ -1,2 +1,6 @@
 let state=null
 console.log(typeof(state)) 
+
+const bigNumber=123415367262n
+
+console.log(typeof bigNumber)
