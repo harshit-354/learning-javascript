ce905442feeb3d9bb6bad5810046a5b9cdc470d8
harshit <harshit.gupta0411@gmail.com>
2026-09-09 03:31:06 +0530

    arrays2 completed

---
+++ basic/arrays2.js
@@ -0,0 +1,26 @@
+// const marvel_heros=["thor","Ironman","spiderman"]
+// const dc_heores=["superman","flash","batman"]
+
+// const allheors=marvel_heros.concat(dc_heores)
+// console.log(allheors)
+
+// //
+// const allnewheros=[...dc_heores,...marvel_heros]
+
+// console.log(allnewheros);
+
+// //
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_another_array=another_array.flat(Infinity)
+// console.log(real_another_array)
+//
+// console.log(Array.isArray("hitesh"))
+
+// console.log(Array.from("harshit"));
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3))
