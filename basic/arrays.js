4034331a6a0e1db637cff5c0cdefada8dfc93694
harshit <harshit.gupta0411@gmail.com>
2026-09-08 20:54:29 +0530

    arrays2

---
+++ basic/arrays.js
@@ -0,0 +1,17 @@
+const myArr=[0,1,2,3,4,5,6]
+myArr.push(663)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
+myArr.shift()
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(3))
+const newarr=myArr.join()
+
+console.log(typeof newarr)
+
+console.log("A",myArr);
+const myn1=myArr.slice(1,3)
+
+console.log()
+
